f22012dadfe1529c56b191254a5f8acf828f2d95
BarentLimita17 <barentlimita11@gmail.com>
2024-03-22 22:43:53 +0700

    fix: responsive bugs fixed

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import { FaPeopleGroup } from "react-icons/fa6";
 import { MdWork } from "react-icons/md";
 import FourStars from "~/components/4stars";
 import FiveStars from "~/components/5stars";
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -32,7 +33,7 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             {/* PRODUCT 1 */}
             <div className="group relative items-center justify-center overflow-hidden hover:shadow-xl hover:shadow-black/30 transition-shadow">
-              <div className="h-96 w-72">
+              <div className="h-[175px] md:h-[384px] md:w-[288px]">
                 <Image
                   src="/home/bg-product1.jpeg" priority alt="Product1 image" className="h-full w-full object-cover group-hover:rotate-[10deg] group-hover:scale-125 transition-transform duration-1000" sizes="100vw" width={0} height={0} style={{ width: '100%', height: '100%' }}
                 />
@@ -47,7 +48,7 @@ export default function Home() {
             </div>
             {/* PRODUCT 2 */}
             <div className="group relative items-center justify-center overflow-hidden hover:shadow-xl hover:shadow-black/30 transition-shadow">
-              <div className="h-96 w-72">
+              <div className="h-[175px] md:h-[384px] md:w-[288px]">
                 <Image loading="lazy"
                   src="/home/bg-product2.jpg" alt="Product2 image" className="h-full w-full object-cover group-hover:rotate-[10deg] group-hover:scale-125 transition-transform duration-1000" sizes="100vw" width={0} height={0} style={{ width: '100%', height: '100%' }}
                 />
@@ -62,7 +63,7 @@ export default function Home() {
             </div>
             {/* PRODUCT 2 */}
             <div className="group relative items-center justify-center overflow-hidden hover:shadow-xl hover:shadow-black/30 transition-shadow">
-              <div className="h-96 w-72">
+              <div className="h-[175px] md:h-[384px] md:w-[288px]">
                 <Image loading="lazy"
                   src="/home/bg-product3.jpg" alt="Product3 image" className="h-full w-full object-cover group-hover:rotate-[10deg] group-hover:scale-125 transition-transform duration-1000" sizes="100vw" width={0} height={0} style={{ width: '100%', height: '100%' }}
                 />
@@ -112,9 +113,16 @@ export default function Home() {
         </div>
         <div className='mb-[3%] text-[12px] md:text-[16px]'>
           <div className='flex flex-wrap text-justify text-white'>
-            <div>
-              <Link href="/about#history">
-                <GiBlackBook size={40} className="text-[#f90334] hover:scale-125 hover:text-black duration-500" />
+            <div className="flex gap-5">
+              <div>
+                <Link href="/about#history">
+                  <GiBlackBook size={40} className="text-[#f90334] hover:scale-125 hover:text-black duration-500" />
+                </Link>
+              </div>
+              <Link className="flex items-center" href="/about#history">
+                <div className="flex items-center justify-start text-[#fa0334] gap-2">
+                  More about History <FaArrowRightLong />
+                </div>
               </Link>
             </div>
             Accelist Aviation's journey began with a vision to revolutionize ground handling operations and aircraft maintenance. Since its inception, the company has remained committed to innovation, quality, and customer satisfaction. With a rich history of 16 years, Accelist Aviation has continually evolved to meet the changing needs of the aviation sector, solidifying its reputation as a leader in the field.
@@ -122,19 +130,33 @@ export default function Home() {
         </div>
         <div className='mb-[3%] text-[12px] md:text-[16px]'>
           <div className='flex flex-wrap justify-end text-justify text-white'>
-            <div>
-              <Link href="/teams">
-                <FaPeopleGroup size={40} className="text-[#f90334]" />
+            <div className="flex gap-5">
+              <Link className="flex items-center" href="/teams">
+                <div className="flex items-center justify-start text-[#fa0334] gap-2">
+                  <FaArrowLeftLong /> More about Teams
+                </div>
               </Link>
+              <div>
+                <Link href="/teams">
+                  <FaPeopleGroup size={40} className="text-[#f90334] hover:scale-125 hover:text-black duration-500" />
+                </Link>
+              </div>
             </div>
             At the heart of Accelist Aviation's success is its dedicated team of professionals, comprising industry experts, engineers, technicians, and customer service specialists. Fueled by passion and expertise, the team at Accelist works tirelessly to deliver cutting-edge solutions that drive efficiency, safety, and operational excellence for their clients.
           </div>
         </div>
         <div className='mb-[3%] text-[12px] md:text-[16px]'>
           <div className='flex flex-wrap text-justify text-white'>
-            <div>
-              <Link href="/about#about">
-                <MdWork size={40} className="text-[#f90334]" />
+            <div className="flex gap-5">
+              <div>
+                <Link href="/about#about">
+                  <MdWork size={40} className="text-[#f90334] hover:scale-125 hover:text-black duration-500" />
+                </Link>
+              </div>
+              <Link className="flex items-center" href="/about#about">
+                <div className="flex items-center justify-start text-[#fa0334] gap-2">
+                  More About our Cultures <FaArrowRightLong />
+                </div>
               </Link>
             </div>
             Accelist Aviation fosters a culture of collaboration, innovation, and integrity. With a focus on teamwork and continuous improvement, the company encourages its employees to push the boundaries of possibility and strive for excellence in everything they do.
@@ -150,30 +172,30 @@ export default function Home() {
               <hr className='text-center text-white h-[5px] w-[78px]' />
             </div>
           </div>
-          <div className="flex flex-col max-w-5xl gap-8 group">
+          <div className="flex flex-col max-w-5xl gap-8 group contrast-150">
             <div className="bg-white/5 group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 duration-500 cursor-pointer p-8 rounded-xl mix-blend-normal">
               <FiveStars />
               <Image loading="lazy" className="mx-auto rounded-full" src='/home/skyrise.jpg' width={100} height={100} alt='skyrise' />
               <div className="uppercase text-xl font-bold">SkyRise Airlines</div>
-              <div className="text-sm leading-7 my-3 font-light opacity-50 text-white">"At SkyRise Airlines, Accelist Aviation is our go-to partner for seamless ground operations. Their top-notch ground support equipment, including aircraft towing tractors and self-propelled passenger stairs, ensures efficient aircraft handling. Accelist's commitment to quality and reliability has earned our trust, making them an integral part of our operations."</div>
+              <div className="text-sm leading-7 my-3 font-medium opacity-50 text-white">"At SkyRise Airlines, Accelist Aviation is our go-to partner for seamless ground operations. Their top-notch ground support equipment, including aircraft towing tractors and self-propelled passenger stairs, ensures efficient aircraft handling. Accelist's commitment to quality and reliability has earned our trust, making them an integral part of our operations."</div>
             </div>
             <div className="bg-white/5 group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 duration-500 cursor-pointer p-8 rounded-xl mix-blend-normal">
               <FourStars />
               <Image loading="lazy" className="mx-auto rounded-full" src='/home/aerotech.jpg' width={100} height={100} alt='aerotech' />
               <div className="uppercase text-xl font-bold">AeroTech Solutions</div>
-              <div className="text-sm leading-7 my-3 font-light opacity-50 text-white">"AeroTech Solutions relies on Accelist Aviation for precision-engineered tools that elevate our maintenance procedures. Their data loaders and testing equipment meet industry standards, streamlining our operations and enhancing safety. Accelist's tools are reliable and durable, contributing to our success."</div>
+              <div className="text-sm leading-7 my-3 font-medium opacity-50 text-white">"AeroTech Solutions relies on Accelist Aviation for precision-engineered tools that elevate our maintenance procedures. Their data loaders and testing equipment meet industry standards, streamlining our operations and enhancing safety. Accelist's tools are reliable and durable, contributing to our success."</div>
             </div>
             <div className="bg-white/5 group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 duration-500 cursor-pointer p-8 rounded-xl mix-blend-normal">
               <FiveStars />
               <Image loading="lazy" className="mx-auto rounded-full" src='/home/globaljet.png' width={100} height={100} alt='globaljet' />
               <div className="uppercase text-xl font-bold">GlobalJet Services</div>
-              <div className="text-sm leading-7 my-3 font-light opacity-50 text-white">"Accelist Aviation's chemicals and parts are essential for maintaining peak performance across our fleet at GlobalJet Services. Their products, from cleaners to filters, consistently deliver reliability. Accelist's dedication to quality ensures our aircraft operate at optimal levels, making them a trusted partner."</div>
+              <div className="text-sm leading-7 my-3 font-medium opacity-50 text-white">"Accelist Aviation's chemicals and parts are essential for maintaining peak performance across our fleet at GlobalJet Services. Their products, from cleaners to filters, consistently deliver reliability. Accelist's dedication to quality ensures our aircraft operate at optimal levels, making them a trusted partner."</div>
             </div>
             <div className="bg-white/5 group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 duration-500 cursor-pointer p-8 rounded-xl mix-blend-normal">
               <FourStars />
               <Image loading="lazy" className="mx-auto rounded-full" src='/home/horizon.png' width={100} height={100} alt='horizon' />
               <div className="uppercase text-xl font-bold">Horizon Airways</div>
-              <div className="text-sm leading-7 my-3 font-light opacity-50 text-white">"At Horizon Airways, Accelist Aviation plays a vital role in our commitment to exceptional service and safety. Their streamlined operations and innovative solutions contribute to our efficiency. Accelist's excellence in ground support equipment and maintenance tools enhances our operations, making them invaluable partners in the aviation industry."</div>
+              <div className="text-sm leading-7 my-3 font-medium opacity-50 text-white">"At Horizon Airways, Accelist Aviation plays a vital role in our commitment to exceptional service and safety. Their streamlined operations and innovative solutions contribute to our efficiency. Accelist's excellence in ground support equipment and maintenance tools enhances our operations, making them invaluable partners in the aviation industry."</div>
             </div>
           </div>
         </div>
